f0acd508bf19417c076a1afe61e3304e7f83c167
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-08-22 14:09:22 -0400

    Favorite color and number application

---
+++ FavoriteColorAndNumber1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteColorAndNumber1
+{
+    //Class: Program
+    //Author: Tyler Amey
+    //Purpose Console Read/Write and Exception-handling exercise
+    //Restrictions: none
+    internal class Program
+    {
+        //Method: Main
+        //Purpose: Prompt user for their favorite color and number
+        //         Output their favorite color (in limit text colors) their favorite number
+        //Restrictions: none
+        static void Main(string[] args)
+        {
+            //prompt user for their favorite color
+
+            //get user's inoput and store it in a variable
+
+            //prompt the user for their favorite number
+
+            //validate that the user entered a valid number
+
+            //change the color of the output text to match the user's favorite color
+
+            //output their favorite color the number of times as their favorite number
+        }
+    }
+}
