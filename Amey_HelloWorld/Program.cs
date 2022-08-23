cd8ddd98826f1d3ffd8deba39f8790ee4e849156
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-08-23 17:16:17 -0400

    PE1

---
+++ Amey_HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amey_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<5; i++)
+            {
+                Console.WriteLine("Tyler Amey");
+            }
+
+            int f = 5;
+            int g = 7;
+            for(int i = 0; i < 5; i++)
+            {
+                f = g * f;
+                Console.WriteLine(f);
+            }
+
+        }
+    }
+}
