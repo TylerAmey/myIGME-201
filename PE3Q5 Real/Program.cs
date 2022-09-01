382bfa42910f0d29f763fb778ea9f7af06b79cf5
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-08-31 21:51:11 -0400

    Question 5

---
+++ PE3Q5 Real/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE3Q5_Real
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 4 INTEGER values SEPPERATED BY A COMMA (ex 1,2,3,4).");
+            string sIntegers = Console.ReadLine();
+            string[] sSplitIntegers = sIntegers.Split(',');
+            int nProduct = 1;
+
+            foreach (string s in sSplitIntegers)
+            {
+                int nInteger = Convert.ToInt32(s);
+                nProduct = nProduct * nInteger;
+            }
+
+            Console.WriteLine(nProduct);
+        }
+    }
+}
---
+++ PE3Q5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE3Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 4 INTEGER values.");
+            string sIntegers = Console.ReadLine();
+            string[] sSplitIntegers = sIntegers.Split(" ");
+            int nProduct = 0;
+
+            foreach (string s in sSplitIntegers)
+            {
+                int nInteger = Convert.ToInt32(s);
+                nProduct = nProduct * nInteger;
+            }
+
+            Console.WriteLine(nProduct);
+
+
+        }
+    }
+}
+ 
