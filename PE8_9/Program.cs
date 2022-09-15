a8c51ec94350673040d447ac0ff58fcc0f443c35
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-09-14 22:06:49 -0400

    Problem 9

---
+++ PE8_9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+            string line = Console.ReadLine();
+            string newLine = "";
+            int start = 0;
+            foreach (string word in line.Split(' '))
+            {
+                string newWord = "";
+                if (start == 0)
+                {
+                    newWord = "\u0022" + word + "\u0022";
+                    newLine = newLine + newWord;
+                    start = 1;
+                }
+                else
+                {
+                    newWord = "\u0022" + word + "\u0022";
+                    newLine = newLine + " " + newWord;
+                }
+            }
+            Console.WriteLine(newLine);
+        }
+    }
+}
