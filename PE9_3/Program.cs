2c1e163b8992963057a8d16ef0c691634da2854c
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-09-17 16:49:34 -0400

    Delegate

---
+++ PE9_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_3
+{
+    static class Program
+    {
+
+        delegate string lineReader(string input);
+        
+        static void Main(string[] args)
+        {
+
+            lineReader line;
+            line = new lineReader(ReadInput);
+
+            string ReadInput(string input)
+            {
+                return input;
+            }
+        }
+    }
+}
