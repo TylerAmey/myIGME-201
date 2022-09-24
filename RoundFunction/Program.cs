c64422a54eca81ed884cf7dfa0511b458c04a86e
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-09-24 16:14:58 -0400

    Round delegate

---
+++ RoundFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundFunction
+{
+    internal class Program
+    {
+
+        delegate double MyRound(double nToRound, int nPlaces);
+        static void Main(string[] args)
+        {
+            MyRound myRound;
+
+            myRound = new MyRound(Math.Round);
+
+            double result = myRound(23.297197, 2);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
