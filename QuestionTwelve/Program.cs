c3343bbb70bc79166d195fef3e2c6cc6c9715eba
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-09-25 21:15:06 -0400

    Question 12

---
+++ QuestionTwelve/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionTwelve
+{
+    static internal class Program
+    {
+        static string sName;
+        static double dSalary = 30000;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            sName = Console.ReadLine();
+            bool bCongrats = GiveRaise(sName, ref dSalary);
+            if (bCongrats == true)
+            {
+                Console.WriteLine("Congratulations " + sName + ", you've been promoted! Your new salary is " + dSalary + "!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry " + sName + ", you did not recieve a raise"); 
+            }
+        }
+
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if(name == "Tyler Amey")
+            {
+                dSalary += 19999.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TestCode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] dArray = new double[2][];
+            dArray[0] = new double[2];
+            dArray[1] = new double[1];
+
+            dArray[0][0] = 15;
+            dArray[0][1] = 5.67;
+
+            Console.WriteLine(dArray);
+
+
+
+
+        }
+    }
+}
