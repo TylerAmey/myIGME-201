3824357b0d82a16c5568890dc051abc3f85f8cfa
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-09-25 21:22:20 -0400

    Question 13

---
+++ QuestionThirteen/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionThirteen
+{
+    internal class Program
+    {
+        struct employee
+        {
+            public string sName;
+            public double dSalary;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            employee employee = new employee();
+            employee.sName = Console.ReadLine();
+            employee.dSalary = 30000;
+            bool bCongrats = GiveRaise(employee);
+            if (bCongrats == true)
+            {
+                Console.WriteLine("Congratulations " + employee.sName + ", you've been promoted! Your new salary is " + employee.dSalary + "!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry " + employee.sName + ", you did not recieve a raise");
+            }
+        }
+
+        static bool GiveRaise(employee employee)
+        {
+            if (employee.sName == "Tyler Amey")
+            {
+                employee.dSalary += 19999.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
