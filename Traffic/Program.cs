9274b347588ed103f31f72d209393929ee8bd743
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-09-29 18:35:06 -0400

    Question6

---
+++ Traffic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclesReal;
+
+namespace Traffic
+{
+    internal class Program
+    {
+
+        static void AddPassenger(VehiclesReal.IPassengerCarrier passenger)
+        {
+            passenger.LoadPassenger();
+            Console.WriteLine(passenger.ToString());
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
