bbf72290074a6acd8117032baa42d9f9c6b95cb8
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-10-18 13:59:20 -0400

    Question 4 Test 2

---
+++ TestForTest2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForTest2
+{
+    SortedList<(double, double, double), double> zFunc = new SortedList<(double, double, double), double>();
+    double dX, dY, dW, dZ;
+    int nX, nY, nW;
+    for()
+    nY = 0;
+    for(dY = -1; dY <=1; dY += 0.1){
+        dY = Math.Round(dY,1);
+        nX = 0;
+        for(dX = 0; dX <=4; dY +=0.1){
+            dX = Math.Round(dX,1);
+            dZ = 4 * dY * dY * dY + 2 * dX * dX - 8 * dX + 7;
+            dX = Math.Round(dZ, 3);
+            zFunc[nX, nY, 0] = dX;
+            zFunc[nX, nY, 1] = dY;
+            zFunc[nX, nY, 2] = dZ;
+            ++nY;
+        }
+        ++nX;
+    }
+
+
+
+}
---
+++ UnitTest2Q4/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest2Q4
+{
+    
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+
+    }
+
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumber()
+        {
+            return this.phoneNumber;
+        }
+
+        public abstract void Connect();
+
+        public abstract void Disconnect();
+    }
+
+
+    public class RotaryPhone : Phone, PhoneInterface
+    {
+        public override void Connect()
+        {
+            Console.WriteLine("rotary connected");
+        }
+
+        public override void Disconnect()
+        {
+            Console.WriteLine("rotary disconnected");
+        }
+
+        public virtual void HangUp()
+        {
+            Console.WriteLine("rotary hang up");
+        }
+
+        public virtual void MakeCall()
+        {
+            Console.WriteLine("rotary make a call");
+        }
+
+        public virtual void Answer()
+        {
+            Console.WriteLine("rotary answer phone");
+        }
+    }
+
+    public class PushButtonPhone : Phone, PhoneInterface
+    {
+        public override void Connect()
+        {
+            Console.WriteLine("push button connected");
+        }
+
+        public override void Disconnect()
+        {
+            Console.WriteLine("push button disconnected");
+        }
+
+        public virtual void HangUp()
+        {
+            Console.WriteLine("push button hang up");
+        }
+
+        public virtual void MakeCall()
+        {
+            Console.WriteLine("push button make a call");
+        }
+
+        public virtual void Answer()
+        {
+            Console.WriteLine("push button answer phone");
+        }
+    }
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewDriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get { return whichDrWho; }
+        }
+
+        public string FemaleSideKick
+        {
+            get { return femaleSideKick; }
+        }
+
+        public void TimeTravel()
+        {
+            Console.WriteLine("Time travel");
+        }
+    }
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+            Console.WriteLine("Opens door");
+        }
+
+        public void CloseDoor()
+        {
+            Console.WriteLine("Closes door");
+        }
+    }
+
+
+}
